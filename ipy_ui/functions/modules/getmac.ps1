61789e9df81d536cfa21dc29804d2be998cba246
Egor Levashov <112577182+sxfour@users.noreply.github.com>
2023-04-24 16:57:56 +0300

    Delete ipy_ui directory

--- ipy_ui/functions/modules/getmac.ps1
+++
@@ -1,7 +0,0 @@
-﻿Function GetMACInfo {
-    $host_mac = (Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object {$_.ipenabled -EQ $true}).Macaddress | select-object -first 1
-
-    return $host_mac
-}
-
-GetMACInfo
